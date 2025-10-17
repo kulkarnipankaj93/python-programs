bad9514d50ac22819a346e175889929ee541f420
kulkarnipankaj93 <kulkarnipankaj93@gmail.com>
2025-10-17 09:16:13 +0530

    length fo string program added

--- Beginner/String/1-reverse string.py
+++ Beginner/String/1-reverse string.py
@@ -1,6 +1,7 @@
-my_string = "Hello World"
-reverse_string = ""
+my_string = "Hello World"  # Given string
+reverse_string = ""        # empty string to store reversed string
 
+# applying for loop
 for i in my_string:
     reverse_string = i + reverse_string
 
---
+++ Beginner/String/2-Length of string.py
@@ -0,0 +1,9 @@
+given_string = "Python Course"   # Given string
+count = 0                        # counter
+
+# Loop through each character in the string 'given_String'
+for char in given_string:
+    # Increment 'count' by 1 for each character
+    count += 1
+
+print("Length of the string is-", count)
