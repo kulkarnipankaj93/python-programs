2cda38c9fa2a4f8e241a4d8f12bb555ccb16afb6
kulkarnipankaj93 <kulkarnipankaj93@gmail.com>
2025-11-01 09:44:51 +0530

    file updates

--- Beginner/General/random_no_list.py
+++ Beginner/General/random_no_list.py
@@ -1,9 +1,10 @@
 import random
 
+# Program to generate random numbers from 1 to 50.
 numbers = [random.randint(1,50) for i in range(10)]
-
 print("Random numbers", numbers)
 
-unique = random.sample(range(1, 51), 10)
 
+# Program to generate Unique random numbers from 1 to 50.
+unique = random.sample(range(1, 51), 10)
 print("Unique random numbers", unique)
